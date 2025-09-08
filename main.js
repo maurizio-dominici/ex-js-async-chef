36cd627ab5bf8fd821146fdd5718e827d771bc3e
maurizio-dominici <mauriziodominici4@gmail.com>
2025-09-08 11:40:11 +0200

    add Bonus 1

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 async function getChefBirthday(id) {
-  const ricettaResponse = await fetch(`https://dummyjson.com/recipes/${id}`);
-  const ricetta = await ricettaResponse.json();
-  //   console.log("Ricetta", ricetta);
-  const userId = ricetta.userId;
-  const userResponse = await fetch(`https://dummyjson.com/users/${userId}`);
-  const user = await userResponse.json();
-  //   console.log("User :", user);
+  let ricetta;
+  try {
+    const ricettaResponse = await fetch(`https://dummyjson.com/recipes/${id}`);
+    ricetta = await ricettaResponse.json();
+    //   console.log("Ricetta", ricetta);
+  } catch (error) {
+    throw new Error(`Non recupero la ricetta id ${id}`);
+  }
+  if (!ricetta) {
+    throw new Error(`Ricetta id ${id} non trovata`);
+  }
+  let user;
+  try {
+    const userId = ricetta.userId;
+    const userResponse = await fetch(`https://dummyjson.com/users/${userId}`);
+    user = await userResponse.json();
+    //   console.log("User :", user);
+  } catch (error) {
+    throw new Error(`Non recupero lo user id ${userId}`);
+  }
+  if (!user) {
+    throw new Error(`User id ${userId} non trovato`);
+  }
   const birthDate = user.birthDate;
   return birthDate;
 }
